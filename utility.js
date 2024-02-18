5da6bd6921158c090c55252914a9c17d185d413c
ShakirEvan530 <ausshakirahmed@gmail.com>
2024-02-19 01:08:15 +0600

    seat remaining part done

---
+++ utility.js
@@ -0,0 +1,42 @@
+const seatNumber = document.getElementById('seatSelected');
+let ticket = parseInt(seatNumber.innerText);
+const soldTicketName = document.getElementById('soldTicket');
+const seatDecrease = document.getElementById('seatRemaining');
+let ticketRemainings = parseInt(seatDecrease.innerText);
+
+function seatName(button){
+  if(ticket===4){
+    alert('You can not buy more than four seats ');
+    return;
+  }
+
+  if(button.getAttribute('data-clicked')==='true'){
+      alert('This seat has already been booked');
+      return;
+  }
+
+  ticketRemainings--;
+  seatDecrease.innerText= ticketRemainings;
+  
+  let paragraph = document.createElement('p')
+  paragraph.innerText=button.id;
+  soldTicketName.appendChild(paragraph);
+  
+  let paragraphTwo= document.createElement('p')
+  paragraphTwo.innerText='Economy';
+  soldTicketName.appendChild(paragraphTwo);
+
+  let paragraphThree = document.createElement('p')
+  paragraphThree.innerText=550;
+  soldTicketName.appendChild(paragraphThree);
+
+  button.setAttribute('data-clicked','true')
+
+  ticket++;
+  seatNumber.innerText=ticket;
+  button.style.backgroundColor ='#1dd100';
+  document.getElementById ('totalCost').innerText= 550*ticket;
+  document.getElementById ('totalGrandCost').innerText= 550*ticket;
+  
+}
+
